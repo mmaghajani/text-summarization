b23086749761311f216210555718f91b17a7d138
mma <mma137421@gmail.com>
2018-06-30 07:26:59 +0430

    Adds tokenize method

--- term-freq-single-doc-summarizer.py
+++ term-freq-single-doc-summarizer.py
@@ -2,6 +2,7 @@ import os
 import pprint
 
 DOCS = dict()
+WORD_DATA = dict()
 
 
 def read_documents():
@@ -15,8 +16,24 @@ def read_documents():
             content = ""
             for line in doc:
                 content += line
+
             fp.close()
         DOCS[filename] = content
 
 
+def tokenize():
+    for filename in DOCS.keys():
+        content = DOCS[filename]
+        words = content.replace("\n", " ").replace(".", " ").replace("،", " "). \
+            replace("!", " ").replace(";", " ").replace(")", " ").replace("(", " "). \
+            split(" ")
+        for word in set(words):
+            if filename not in WORD_DATA.keys():
+                WORD_DATA[filename] = {word: words.count(word)}
+            else:
+                WORD_DATA[filename].update({word: words.count(word)})
+
+
 read_documents()
+tokenize()
+pprint.pprint(WORD_DATA)
