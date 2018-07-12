07c45420560dcc2d83434ec6b5c4c80e440227a4
mma <mma137421@gmail.com>
2018-07-13 03:05:32 +0430

    Implements sparse-coding algo.

--- term-freq-single-doc-summarizer.py
+++ term-freq-single-doc-summarizer.py
@@ -2,6 +2,9 @@ import math
 import os
 import pprint
 import re
+import numpy as np
+from numpy import linalg as LA
+import pandas as pd
 
 DOCS = dict()
 WORD_DATA = dict()
@@ -28,68 +31,90 @@ def read_document(doc_name):
     return sentences, words
 
 
-def tokenize():
-    for filename in DOCS.keys():
-        content = DOCS[filename]
-        words = content.replace("\n", " ").replace(":", " ").replace("»", " ").replace("'", " ").\
-            replace(".", " ").replace("،", " ").replace("«", " "). \
-            replace("!", " ").replace(";", " ").replace(")", " ").replace("(", " "). \
-            split(" ")
-        for word in set(words):
-            if word not in WORD_DATA.keys():
-                WORD_DATA[word] = {filename: words.count(word)}
+def make_term_frequency(sentences, words):
+    term_frequency = dict()
+    for sentence in sentences:
+        vector = list()
+        for i in range(0, len(words)):
+            word = words[i]
+            vector.append(sentence.count(word))
+        term_frequency[sentence] = vector
+    return term_frequency
+
+
+def sparse_coding(S, summary, lam):
+    n = S.shape[1]
+    k = summary.shape[1]
+    A_current = np.full((k, n), 1/k)
+    lam1 = np.full((k, n), lam)
+    t = 0
+    while t < 100:
+        A_new = np.divide(np.multiply(A_current, np.matmul(summary.transpose(), S)),
+                          np.add(np.matmul(np.matmul(summary.transpose(), summary), A_current), lam1))
+        if LA.norm(A_new - A_current) < 0.01:
+            break
+        t += 1
+        A_current = A_new
+    return A_current
+
+
+def J(S, A, summary_current, lam):
+    loss1 = 0
+    loss2 = 0
+    for i in range(len(S)):
+        prediction = 0
+        for j in range(len(summary_current)):
+            prediction += A[j][i] * np.array(S[j])
+
+        loss1 += LA.norm(np.array(S[i])-prediction)      # L2-norm
+        loss2 += LA.norm(np.array(A[:, i]), ord=1)  # L1-norm of a:i
+
+    return loss1+(lam*loss2)
+
+
+def update_summary(s, T):
+    return ''
+
+
+def Accept(s, tmp, summary_current, T):
+    return False
+
+
+def update_T(step):
+    return ''
+
+
+def MDS_sparse(S, k, lam, Tstop, MaxConseRej):
+    summary_current = pd.DataFrame()    # initialized randomly
+    T = 0
+    rej = 0
+    Jopti = 9999999 #max
+    summary_opti = pd.DataFrame()
+    step = 0
+    while T > Tstop:
+        A = sparse_coding(S, summary_current, lam)
+        current_J = J(S, A, T, lam)
+        if current_J < Jopti:
+            Jopti = current_J
+            summary_opti = summary_current
+        else:
+            rej += 1
+            if rej >= MaxConseRej:
+                return summary_opti
+        summary_new = pd.DataFrame()
+        for s in summary_current:
+            tmp = update_summary(s, T)
+            if Accept(s, tmp, summary_current, T):
+                summary_new.add(tmp)
             else:
-                WORD_DATA[word].update({filename: words.count(word)})
-
-    for word in WORD_DATA.keys():
-        count = 0
-        for num in WORD_DATA[word].values():
-            count += num
-        WORD_DATA[word].update({"all": count})
-
-
-def mutual_info():
-    MIs = dict()
-    N = 100
-    score = dict()
-    for word in WORD_DATA.keys():
-        Nw = WORD_DATA.get(word)["all"]
-        Nwbar = N - Nw
-        for doc in DOCS.keys():
-            Ni = len(DOCS.get(doc))
-            Niw = WORD_DATA.get(word)[doc]
-            Niwbar = Ni - Niw
-            Nibar = N - Ni
-            Nibarw = Nw - Niw
-            Nibarwbar = Nibar - Nibarw
-            a = 0.0000000000
-            b = 0.0000000000
-            c = 0.0000000000
-            d = 0.0000000000
-            try:
-                if Niw is not 0:
-                    a = (Niw / N) * math.log2((N * Niw) / (Nw * Ni))
-                if Niwbar is not 0:
-                    b = (Niwbar / N) * math.log2((N * Niwbar) / (Nwbar * Ni))
-                if Nibarw is not 0:
-                    c = (Nibarw / N) * math.log2((N * Nibarw) / (Nw * Nibar))
-                if Nibarwbar is not 0:
-                    d = (Nibarwbar / N) * math.log2((N * Nibarwbar) / (Nwbar * Nibar))
-            except ValueError as e:
-                print(N , Niw , Ni , Niwbar , Nwbar, Nibarwbar, Nibar)
-            MI = a + b + c + d
-            if word not in MIs.keys():
-                MIs[word] = {doc: MI}
-            else:
-                MIs.get(word).update({doc: MI})
-        s = 0
-        for doc in DOCS.keys():
-            Ni = len(DOCS.get(doc))
-            Pci = Ni / N
-            s += (MIs.get(word)[doc] * Pci)
-        score[word] = s
+                summary_new.add(s)
+        summary_current = summary_new
+        T = update_T(step)
+        step += 1
+    return summary_opti
 
 
 sentences, words = read_document('ALF.CU.13910117.019.txt')
-print(sentences, "\n", words)
-# tokenize()
+listed_word = list(words)
+print(listed_word)
+term_frequency = make_term_frequency(sentences, listed_word)
