7513e8a5efc31f39a6f8af0d0d116bf9dc174a51
mma <mma137421@gmail.com>
2018-07-17 01:03:38 +0430

    Completes multi term frequency

--- term-freq-multi-doc-summarizer.py
+++ term-freq-multi-doc-summarizer.py
@@ -1,88 +1,116 @@
-import math
 import os
-import pprint
+import re
+from lxml import etree
+import sparse_coding as sc
+import evaluation as eval
+import numpy as np
+from evaluation import Level
+import fnmatch
 
 DOCS = dict()
 WORD_DATA = dict()
 
+NUMBER_SUMMARY_SET_ELEMENT = 5
+LAMBDA = 3
+TSTOP = 0.0001
+MAX_CONSE_REJ = 100
 
-def read_documents():
-    directory_path = "data/Multi/Track1/Source/D91A01/"
+
+def read_documents(directory_path):
+    # directory_path = "data/Multi/Track1/Source/D91A01/"
     directory = os.fsencode(directory_path)
-
+    contents = ""
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        with open(directory_path + filename) as fp:
-            doc = fp.readlines()
-            content = ""
-            for line in doc:
-                content += line
+        content = etree.parse(directory_path + filename)
+        memoryElem = content.find('TEXT')
+        DOCS[filename] = memoryElem.text
+        contents += memoryElem.text
 
-            fp.close()
-        DOCS[filename] = content
+    sentences = re.split("\.|\?|\!", contents)
+    while sentences.__contains__(''):
+        sentences.remove('')
 
-
-def tokenize():
-    for filename in DOCS.keys():
-        content = DOCS[filename]
-        words = content.replace("\n", " ").replace(":", " ").replace("»", " ").replace("'", " ").\
-            replace(".", " ").replace("،", " ").replace("«", " "). \
-            replace("!", " ").replace(";", " ").replace(")", " ").replace("(", " "). \
-            split(" ")
-        for word in set(words):
-            if word not in WORD_DATA.keys():
-                WORD_DATA[word] = {filename: words.count(word)}
-            else:
-                WORD_DATA[word].update({filename: words.count(word)})
-
-    for word in WORD_DATA.keys():
-        count = 0
-        for num in WORD_DATA[word].values():
-            count += num
-        WORD_DATA[word].update({"all": count})
+    words = list(set(map(lambda x: x.strip(), contents.replace("?", " ").replace("!", " ").replace(".", " ").
+                         replace("؟", " ").replace("!", " ").replace("،", " ").split(" "))))
+    if words.__contains__(''):
+        words.remove('')
+    return sentences, words
 
 
-def mutual_info():
-    MIs = dict()
-    N = 100
-    score = dict()
-    for word in WORD_DATA.keys():
-        Nw = WORD_DATA.get(word)["all"]
-        Nwbar = N - Nw
-        for doc in DOCS.keys():
-            Ni = len(DOCS.get(doc))
-            Niw = WORD_DATA.get(word)[doc]
-            Niwbar = Ni - Niw
-            Nibar = N - Ni
-            Nibarw = Nw - Niw
-            Nibarwbar = Nibar - Nibarw
-            a = 0.0000000000
-            b = 0.0000000000
-            c = 0.0000000000
-            d = 0.0000000000
-            try:
-                if Niw is not 0:
-                    a = (Niw / N) * math.log2((N * Niw) / (Nw * Ni))
-                if Niwbar is not 0:
-                    b = (Niwbar / N) * math.log2((N * Niwbar) / (Nwbar * Ni))
-                if Nibarw is not 0:
-                    c = (Nibarw / N) * math.log2((N * Nibarw) / (Nw * Nibar))
-                if Nibarwbar is not 0:
-                    d = (Nibarwbar / N) * math.log2((N * Nibarwbar) / (Nwbar * Nibar))
-            except ValueError as e:
-                print(N , Niw , Ni , Niwbar , Nwbar, Nibarwbar, Nibar)
-            MI = a + b + c + d
-            if word not in MIs.keys():
-                MIs[word] = {doc: MI}
-            else:
-                MIs.get(word).update({doc: MI})
-        s = 0
-        for doc in DOCS.keys():
-            Ni = len(DOCS.get(doc))
-            Pci = Ni / N
-            s += (MIs.get(word)[doc] * Pci)
-        score[word] = s
+def make_term_frequency(sentences, words):
+    term_frequency = dict()
+    for sentence in sentences:
+        vector = list()
+        for i in range(0, len(words)):
+            word = words[i]
+            vector.append(sentence.count(word))
+        term_frequency[sentence] = vector
+    return term_frequency
 
 
-read_documents()
-tokenize()
+def make_summary_as_text(summary_set, term_frequency):
+    summary_text = list()
+    for sentence in term_frequency.keys():
+        if term_frequency[sentence] in summary_set:
+            summary_text.append(sentence)
+    return summary_text
+
+
+def findInSubdirectory(filename, subdirectory="data/Multi"):
+    if subdirectory:
+        path = subdirectory
+    else:
+        path = os.getcwd()
+    for root, dirs, names in os.walk(path):
+        if filename in names:
+            return os.path.join(root, filename)
+    return 'File not found'
+
+
+def readSummaries(dir):
+    summaries = []
+    rootdir1 = "data/Multi"
+    for i in range(1, 9):
+        subDir = os.path.join(rootdir1, "Track" + str(i) + "/Summ/")
+        for root, dirs, files in os.walk(subDir):
+            for file in files:
+                if dir+'.E' in file:
+                    with open(findInSubdirectory(file)) as fp:
+                        doc = fp.readlines()
+                        content = ""
+                        for line in doc:
+                            content += line
+                        fp.close()
+                    summaries.append(content)
+
+    return summaries
+
+
+rootdir = "data/Multi"
+for i in range(1,9):
+    subDir = os.path.join(rootdir, "Track"+str(i)+"/Source/")
+    for root, dirs, files in os.walk(subDir):
+        if dirs:
+            for dir in dirs:
+                print(dir)
+                print(subDir+dir+"/")
+                sentences, words = read_documents(subDir+dir+"/")
+                print(sentences)
+                reference_summaries = readSummaries(dir)
+                term_frequency = make_term_frequency(sentences, words)
+                candidate_set = np.array(list([*v] for k, v in term_frequency.items()))
+                summary_set = sc.MDS_sparse(candidate_set, NUMBER_SUMMARY_SET_ELEMENT, LAMBDA, TSTOP, MAX_CONSE_REJ)
+                summary_text = make_summary_as_text(summary_set, term_frequency)
+                rouge_1_fscores = 0
+                rouge_2_fscores = 0
+                for summary_ref in reference_summaries:
+                    rouge_1_fscore = eval.RougeFScore(summary_text, summary_ref, Level.Rouge_1)
+                    rouge_1_fscores += rouge_1_fscore
+
+                    rouge_2_fscore = eval.RougeFScore(summary_text, summary_ref, Level.Rouge_2)
+                    rouge_2_fscores += rouge_2_fscore
+
+                print("Rouge-1 Fscore : ", rouge_1_fscores / 5)
+                print("Rouge-2 Fscore : ", rouge_2_fscores / 5)
+                # print(result)
