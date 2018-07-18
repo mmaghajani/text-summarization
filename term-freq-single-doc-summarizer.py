e76e72c1da8c7bc03dd4e9af578645dc8920982b
mma <mma137421@gmail.com>
2018-07-18 09:58:31 +0430

    Adds AVG for precision and recall

--- term-freq-single-doc-summarizer.py
+++ term-freq-single-doc-summarizer.py
@@ -13,6 +13,12 @@ MAX_CONSE_REJ = 100
 
 
 directory = os.fsencode(util.DATA_PATH)
+rouge1_fscores_list = list()
+rouge2_fscores_list = list()
+rouge1_precisions_list = list()
+rouge2_precisions_list = list()
+rouge1_recalls_list = list()
+rouge2_recalls_list = list()
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     sentences, words = util.read_document(filename)
@@ -25,14 +31,40 @@ for file in os.listdir(directory):
         summary_text = util.summary_vector_to_text_as_list(summary_set, term_frequency)
         rouge_1_fscores = 0
         rouge_2_fscores = 0
+        rouge_1_precisions = 0
+        rouge_2_precisions = 0
+        rouge_1_recalls = 0
+        rouge_2_recalls = 0
         for summary_ref in reference_summaries:
-            rouge_1_fscore = eval.RougeFScore(summary_text,  summary_ref, Level.Rouge_1)
+            rouge_1_fscore = eval.rouge_Fscore(summary_text,  summary_ref, Level.Rouge_1)
             rouge_1_fscores += rouge_1_fscore
+            rouge_1_precision = eval.rouge_precision(summary_text,  summary_ref, Level.Rouge_1)
+            rouge_1_precisions += rouge_1_precision
+            rouge_1_recall = eval.rouge_recall(summary_text,  summary_ref, Level.Rouge_1)
+            rouge_1_recalls += rouge_1_recall
 
-            rouge_2_fscore = eval.RougeFScore(summary_text,  summary_ref, Level.Rouge_2)
+            rouge_2_fscore = eval.rouge_Fscore(summary_text,  summary_ref, Level.Rouge_2)
             rouge_2_fscores += rouge_2_fscore
+            rouge_2_precision = eval.rouge_precision(summary_text,  summary_ref, Level.Rouge_2)
+            rouge_2_precisions += rouge_2_precision
+            rouge_2_recall = eval.rouge_recall(summary_text,  summary_ref, Level.Rouge_2)
+            rouge_2_recalls += rouge_2_recall
 
         print("Rouge-1 Fscore : ", rouge_1_fscores/5)
         print("Rouge-2 Fscore : ", rouge_2_fscores/5)
+        print("------------------------------------")
+        rouge1_fscores_list.append(rouge_1_fscores/5)
+        rouge2_fscores_list.append(rouge_2_fscores/5)
+        rouge1_precisions_list.append(rouge_1_precisions/5)
+        rouge2_precisions_list.append(rouge_2_precisions/5)
+        rouge1_recalls_list.append(rouge_1_recalls/5)
+        rouge2_recalls_list.append(rouge_2_recalls/5)
     except ValueError:
         print("Sample larger than population")
+
+print("Rouge-1 FScore Avg       : ", sum(rouge1_fscores_list)/len(rouge1_fscores_list))
+print("Rouge-2 FScore Avg       : ", sum(rouge2_fscores_list)/len(rouge2_fscores_list))
+print("Rouge-1 Precision Avg    : ", sum(rouge1_precisions_list)/len(rouge1_precisions_list))
+print("Rouge-2 Precision Avg    : ", sum(rouge2_precisions_list)/len(rouge2_precisions_list))
+print("Rouge-1 Recall Avg       : ", sum(rouge1_recalls_list)/len(rouge1_recalls_list))
+print("Rouge-2 Recall Avg       : ", sum(rouge2_recalls_list)/len(rouge2_recalls_list))
